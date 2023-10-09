9ffdbf7269e35180d81b9aeb03b824849c99e53a
Monty Foster <egymf3@nottingham.ac.uk>
2023-10-09 12:11:36 +0100

    Started Chapter 5 in example sheet

---
+++ Week 1/Hello_World/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+
+    int number_1 = 6;
+    //float number_2 = 5.55;
+    printf(number_1);
+
+    //printf(number_2);
+    //printf(number_3);
+    printf("hello the World\n");
+}
