a70f1578e5715e10755b001f017e875b36abba54
Monty Foster <egymf3@nottingham.ac.uk>
2023-10-09 16:46:23 +0100

    NEw Upload Before closing

--- Week 1/Chap_5_Ex/Ex1-.c
+++ Week 1/Chap_5_Ex/Ex1-.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(void)
 {
+    /* Chapter 5 exercise 1-3
+    
     // Declare some variables
     int a = 1, b = 2;
     float f = 1.23f;
@@ -12,4 +15,14 @@ int main(void)
 
     // Exit from main
     return 0;
+    */
+
+   float r = 10.1; // floast buace the radius is not an int
+   float Volume, Surface;
+
+   Volume = (4*M_PI*r*r*r)/3;
+   Surface = 4*M_PI*r*r;
+
+   printf("The surface area =%f\nThe volume =%f\n",Surface,Volume);
+
 }
